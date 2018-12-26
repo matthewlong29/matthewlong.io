7b2c635d593d274bb1bc5d8a13a18758f6f047a7
Matthew Long <long.matthew29@gmail.com>
2018-12-26 16:49:43 -0500

    [added] admin route

--- app.py
+++ app.py
@@ -24,6 +24,11 @@ def game():
     return render_template("game.html")
 
 
+@app.route("/admin")
+def admin():
+    return render_template("admin.html")
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
