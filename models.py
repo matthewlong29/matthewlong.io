fbd7f29981e91f4b52d31f4fc2d7becc52265bca
Matthew Long <long.matthew29@gmail.com>
2018-12-27 09:30:02 -0500

    [updated] login form

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ DATABASE = SqliteDatabase("articles.db")
 
 class User(UserMixin, Model):
     username = CharField(unique=True)
+    email = CharField(unique=True)
     name = CharField(unique=True)
     password = CharField(max_length=100)
     joined_at = DateTimeField(default=datetime.datetime.now)
@@ -22,6 +23,17 @@ class User(UserMixin, Model):
     def getArticles(self):
         """Get all published articles."""
         return Article.select().where(Article.user == self)
+
+    @classmethod
+    def create_user(cls, username, email, password, admin=False):
+        try:
+            cls.create(
+                username=username,
+                email=email,
+                password=generate_password_hash(password),
+                is_admin=admin)
+        except IntegrityError:
+            raise ValueError("User already exists")
 
 
 class Article(Model):
