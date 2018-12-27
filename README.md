4d9f1e5112a61fb16af01dcaf2772a05d5ff861d
Matthew Long <long.matthew29@gmail.com>
2018-12-26 19:09:53 -0500

    [working] on login view

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # matthewlong.io
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+- pip install flask
+- pip install flask_bcrypt
+- pip install flask_login
+- pip install peewee
+- pip install flask-wtf
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash, redirect
+from forms import LoginForm
 
 app = Flask(__name__)
 app.secret_key = 'kudhgfbiugousifdgjhbsdfjhbsdfjhbjskhbdff98ys98dfs9dufhgisdfbjghuki'
@@ -24,9 +25,14 @@ def game():
     return render_template("game.html")
 
 
-@app.route("/admin")
-def admin():
-    return render_template("admin.html")
+@app.route("/login")
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/')
+    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route("/")
