36f6214d51b117153e21ea83b7ac4078789a61e3
Matthew Long <long.matthew29@gmail.com>
2018-12-27 11:05:12 -0500

    [removed] register class and updated imports

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
 from wtforms.validators import DataRequired, Regexp, ValidationError, Email, Length, EqualTo
 
@@ -15,38 +15,6 @@ def email_exists(form, field):
         raise ValidationError('User with that email already exists.')
 
 
-class RegisterForm(Form):
-    username = StringField(
-        'Username',
-        validators=[
-            DataRequired(),
-            Regexp(
-                r'^[a-zA-Z0-9_]+$',
-                message=("Username should be one word, letters, "
-                         "numbers, and underscores only.")
-            ),
-            name_exists
-        ])
-    email = StringField(
-        'Email',
-        validators=[
-            DataRequired(),
-            Email(),
-            email_exists
-        ])
-    password = PasswordField(
-        'Password',
-        validators=[
-            DataRequired(),
-            Length(min=2),
-            EqualTo('password2', message='Passwords must match')
-        ])
-    password2 = PasswordField(
-        'Confirm Password',
-        validators=[DataRequired()]
-    )
-
-
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
