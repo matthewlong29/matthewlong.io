70cc472d44e7efdd2b7b53a88154dac1d6b05bda
Matthew Long <long.matthew29@gmail.com>
2021-01-17 00:07:06 -0500

    added styles to dotio component scss to space content vertically

--- src/app/dotio/dotio.component.ts
+++ src/app/dotio/dotio.component.ts
@@ -3,9 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   // tslint:disable-next-line: component-selector
   selector: 'matthew-long-dot-io',
-  template: `<dotio-navigation></dotio-navigation>
-             <dotio-center></dotio-center>
-             <dotio-footer></dotio-footer>`
+  styleUrls: ['./dotio.component.scss'],
+  template: `<main>
+               <dotio-navigation></dotio-navigation>
+               <dotio-center></dotio-center>
+               <dotio-footer></dotio-footer>
+             </main>`
 })
 export class DotioComponent implements OnInit {
   constructor() { }
