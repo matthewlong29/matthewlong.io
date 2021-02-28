81324086651e6cf1c8e1bcd51655b65ecbde64f0
Matthew Long <long.matthew29@gmail.com>
2021-02-27 22:37:10 -0500

    removed routes

--- src/app/dotio/dotio-routing.module.ts
+++ src/app/dotio/dotio-routing.module.ts
@@ -1,30 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BioComponent } from './pages/bio/bio.component';
-import { HomeComponent } from './pages/home/home.component';
-import { UsesComponent } from './pages/uses/uses.component';
-import { PolicyListComponent } from '../policy-list/policy-list.component';
+import { DotioComponent } from './dotio.component';
 
 const routes: Routes = [
   {
-    path: 'crud',
-    component: PolicyListComponent
-  },
-  {
-    path: 'uses',
-    component: UsesComponent
-  },
-  {
-    path: 'bio',
-    component: BioComponent
-  },
-  {
-    path: 'home',
-    component: HomeComponent
-  },
-  {
     path: '**',
-    component: HomeComponent
+    component: DotioComponent
   }
 ];
 
